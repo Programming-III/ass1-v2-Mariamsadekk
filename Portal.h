c614df41c9401804190db795ee6ab0e8c38001a5
Mariam Sadek <52850850+Mariamsadekk@users.noreply.github.com>
2025-10-28 11:08:45 +0300

    Update Portal.h

--- Portal.h
+++ Portal.h
@@ -5,7 +5,44 @@
 #include "Instructor.h"
 
 //define class portal here
+#include "Student.h"
 
+
+class Portal {
+private:
+Course** courses;
+int coursesCount;
+int coursesCapacity;
+
+
+Student** students;
+int studentsCount;
+int studentsCapacity;
+
+
+Instructor** instructors;
+int instructorsCount;
+int instructorsCapacity;
+public:
+Portal();
+~Portal();
+
+
+// initialization
+void initializePortal();
+
+
+// dynamic add
+void addCourse(Course* c);
+void addStudent(Student* s);
+void addInstructor(Instructor* i);
+
+
+bool enrollStudentInCourse(Student* s, Course* c);
+
+
+void displayPortalInfo() const;
+};
 
 
 
